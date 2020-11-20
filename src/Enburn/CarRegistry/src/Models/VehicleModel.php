00302178e88dcac01140d8ad2d6455b8a7ba063f
Åsbjørn Hansen <aasbjorn.hansen@gmail.com>
2020-11-20 17:26:25 +0100

    Add base models, controllers and database-classes

---
+++ src/Enburn/CarRegistry/src/Models/VehicleModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Enburn\CarRegistry\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VehicleModel extends Model
+{
+    use HasFactory;
+}
