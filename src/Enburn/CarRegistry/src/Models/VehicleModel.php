a95c7fb23eeffff7cfd94b56a79d3cd3a2eacc90
Åsbjørn Hansen <aasbjorn.hansen@gmail.com>
2020-11-20 18:41:47 +0100

    Use softdelete

--- src/Enburn/CarRegistry/src/Models/VehicleModel.php
+++ src/Enburn/CarRegistry/src/Models/VehicleModel.php
@@ -4,8 +4,10 @@ namespace Enburn\CarRegistry\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VehicleModel extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 }
