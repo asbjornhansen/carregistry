0ae14c226f9a9c9d32fa34d6302766d877afd8dd
Åsbjørn Hansen <aasbjorn.hansen@gmail.com>
2020-11-20 18:41:55 +0100

    Add routes

--- src/Enburn/CarRegistry/src/Routes/api.php
+++ src/Enburn/CarRegistry/src/Routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use Enburn\CarRegistry\Http\Controllers\BrandController;
+use Enburn\CarRegistry\Http\Controllers\VehicleController;
+use Enburn\CarRegistry\Http\Controllers\VehicleModelController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/api', function () {
-    dd('getgot');
+Route::middleware('api')->prefix('api/v1')->group(function () {
+    Route::apiResources([
+        'vehicles' => VehicleController::class,
+        'brands' => BrandController::class,
+        'models' => VehicleModelController::class,
+    ]);
 });
